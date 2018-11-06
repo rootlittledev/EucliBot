2f2c52b4de0db66041690cc22cb59a8c3aa801f2
rootlittledev <qwentin1997@gmail.com>
2018-11-06 16:18:08 +0200

    eucli-bot_v8

--- src/main/java/com/ethero/bot/euclibot/EucliBotApplication.java
+++ src/main/java/com/ethero/bot/euclibot/EucliBotApplication.java
@@ -13,7 +13,7 @@ public class EucliBotApplication {
         Interaction bot = new Interaction();
 
         bot.connect();
-        bot.joinChannel("forsen");
+        bot.joinChannel("GoldenRevolution");
         bot.start();
     }
 }
